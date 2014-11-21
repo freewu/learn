609de6fda2437aa065ec31572d8ae61923f99305
freewu <bluefrog.wu@gmail.com>
2014-11-22 01:40:23 +0800

    使用 map[string]interface{} 来处理json

--- module/encoding/json.go
+++ module/encoding/json.go
@@ -33,7 +33,7 @@ type TestJson struct {
 }
 
 func main() {
-	// encode
+	// encode struct
 	// stuct 不能小写的。。。
 	j := TestJson {
 		A : 1,
@@ -48,7 +48,7 @@ func main() {
 	//os.Stdout.Write(b)
 	fmt.Println(string(b))  // {"A":1,"B":"x","C":[1,2,3]}
 
-	// decode
+	// decode stuct
 	// 字符要处理成byte
 	b1 := []byte(`{"A":1,"B":"x","C":[1,2,3]}`)
 	var j1 TestJson
@@ -59,4 +59,47 @@ func main() {
 	}
 	fmt.Printf("%+v\r\n",j1)
 	fmt.Printf("%x\r\n",j1)
+
+	// encode map
+	m := map[string]int{"apple":5,"pear":6}
+	mb,err := json.Marshal(m)
+	if err != nil {
+		fmt.Println("error: ",err)
+		return
+	}
+	fmt.Println(string(mb)) // {"apple":5,"pear":6}
+	// decode map
+	//var md map[string]int;
+	md := make(map[string]int)
+	mb1 := []byte(`{"apple":5,"pear":6}`)
+	err = json.Unmarshal(mb1,&md)
+	if err != nil {
+		fmt.Println("error: ",err)
+		return
+	}
+	fmt.Printf("%+v\r\n",md)
+
+	// map[string]interface{}
+	m3 := make(map[string]interface{})
+	m3["a"] = 1
+	m3["b"] = "x"
+	m3["c"] = []int{1,2,3}
+	fmt.Printf("%+v\r\n",m3)
+
+	// encode
+	mb3,err := json.Marshal(m3);
+	if err != nil {
+		fmt.Println("error: ",err)
+		return
+	}
+	fmt.Println(string(mb3))
+
+	// decode
+	md3 := make(map[string]interface{})
+	err = json.Unmarshal(mb3,&md3)
+	if err != nil {
+		fmt.Println("error: ",err)
+		return
+	}
+	fmt.Printf("%+v\r\n",md3)
 }
